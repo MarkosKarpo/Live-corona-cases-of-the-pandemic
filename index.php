057547341423c8683416625bf7338b26692660e8
Markos Karpodinis <62939225+MarkosKarpo@users.noreply.github.com>
2020-06-20 18:26:08 +0300

    It's corona time !

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <html>
 <head>
-    <style>
-        .words{
-            font-family: "Open Sans Semibold", fantasy;
-            color: #007bff;
-        }
-    </style>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -15,52 +9,63 @@
 </head>
 
 <div class="container" style = "margin-top : 100px">
-    <h2 class="text-center">COVID-19 PANDEMIC</h2>
+    <h2 class="text-center">COVID-19 CORONAVIRUS PANDEMIC</h2>
 
     <form method = "post" style = "margin-top : 50px">
-        <input type ="text" name= "countryname" placeholder = "Enter Country Name" class="form-control" style = "color : #000000">
+        <input type = text name = "countryname" placeholder = "Enter Country Name" class="form-control" style = "color : #0088ff">
         <br>
         <input type = submit name="submit" class="form-control btn-primary">
     </form>
 </div>
+
+<div class="container text-center" style = "position : absolute ; top : 75% ; left : 8%">
+    <h2 class='btn btn-success'>Stay Home | Stay Safe | Save Lives</h2>
+    <br>
+    <br>
+    <h3>Designed And Developed By Pankaj Panjwani</h3>
+
 </div>
 </body>
 </html>
 <?php
 
+if(isset($_POST['submit']))
+{
 
-    if(!empty($_POST['countryname'])){
-        $str = $_POST['countryname'];
-    }
+    abc($_POST['countryname']);
+}
+function abc($str)
+{
 
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,'https://www.worldometers.info/coronavirus/country/'.$str);
-    curl_setopt($ch,CURLOPT_POST,TRUE);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch,CURLOPT_POST,false);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
     $html = curl_exec($ch);
     curl_close($ch);
+
+
+
 
     $DOM = new DOMDocument;
     libxml_use_internal_errors(true);
     $DOM->loadHTML($html);
 
+//$elements = $DOM->getElementsByClassName("maincounter-number");
     $elements = $DOM->getElementsByTagName('span');
 
+
+
+    /*for($i=4;$i<7;$i++)
+    {
+     echo $elements->item($i)->nodeValue . "<br>";
+    }*/
     echo "<br><br><br><div class='container text-center' style = 'font-weight : bold'>";
-    echo "<span style class='words'>CoronaVirus Cases : </span>" . $elements->item(4)->nodeValue . "<br>";
-
-    echo "<span style class='words'>Deaths : </span>" . $elements->item(5)->nodeValue . "<br>";
-    echo "<span style class='words'>Recovered : </span>" . $elements->item(6)->nodeValue . "<br>";
+    echo "<span style = 'color : #0088ff'>CoronaVirus Cases : </span>" . $elements->item(4)->nodeValue . "<br>";
+    echo "<span style = 'color : #0088ff'>Deaths : </span>" . $elements->item(5)->nodeValue . "<br>";
+    echo "<span style = 'color : #0088ff'>Recovered : </span>" . $elements->item(6)->nodeValue . "<br>";
     echo "<div>";
-
-//$data = $dom->find('div[class=maincounter-number] span');
-//
-//    echo "Coronavirus Cases: ".$data[0]->plaintext."<br>";
-//    echo "Deaths: ". $data[1]->plaintext."<br>";
-//    echo "Recovered: ". $data[2]->plaintext."<br>";
+}
 
 ?>
-
-
